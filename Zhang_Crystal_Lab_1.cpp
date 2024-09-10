75fe6dbe0f559cfe534c457e2c18539cf8276022
Crystal Zhang <crystalz16@nycstudents.net>
2024-09-10 17:35:46 -0400

    Zhang_Crystal_Lab_1

---
+++ Zhang_Crystal_Lab_1.cpp
@@ -0,0 +1,45 @@
+//Crystal Zhang
+//09.10.2024
+//Lab 1
+
+#include <iostream>
+
+using namespace std; 
+
+int main() 
+{
+    cout << "#####################################################################################" << endl; //hashtag
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "# Ways to access the Task Manager on your Windows computer:                         #" << endl; //start of body
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#           Press the key combination Ctrl + Shift + Escape                         #" << endl; //space and hashtag
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#           Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl; //space and hashtag
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#           Type “Task Manager” in the Windows Start menu search                    #" << endl; //end of body
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#                                                                                   #" << endl; //space and hashtag
+    cout << "#####################################################################################" << endl; //hashtag
+
+    return 0; //return code
+
+    /* 
+    putting 
+    "#####################################################################################
+     #                                                                                   #
+     #                                                                                   #
+     # Ways to access the Task Manager on your Windows computer:                         #
+     #                                                                                   #
+     #           Press the key combination Ctrl + Shift + Escape                         #
+     #                                                                                   #
+     #           Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+     #                                                                                   #
+     #           Type “Task Manager” in the Windows Start menu search                    #
+     #                                                                                   #
+     #                                                                                   #
+     #####################################################################################"
+     as an output in a c++ program       
+    */
+
+}
